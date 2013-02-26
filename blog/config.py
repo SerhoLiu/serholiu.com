2dff07a3fa6a2749f5b41a11f992ec1d7b08589d
Serho Liu <serholiu@gmail.com>
2013-02-26 15:36:44 +0800

    Add dev-db and config.

--- blog/config.py
+++ blog/config.py
@@ -4,7 +4,7 @@
 import os.path
 
 COOKIE_SECRET = "11oEnhiuvDTRd_0AGaYdkL5gEmGeJJ-(g7EQnp2XdTP1o/Vo="
-DATABASE = os.path.join(os.path.dirname(__file__), "blog.db")
+DATABASE = os.path.join(os.path.dirname(__file__), "newblog.db")
 
 SITE_NAME = u"I'm SErHo"
 PICKY_DIR = os.path.join(os.path.dirname(__file__), "picky")
