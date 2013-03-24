cdd4ae9a10063ae827ace6410ffad86dd14afc99
Serho Liu <serholiu@gmail.com>
2013-03-24 20:36:41 +0800

    update config.py, add DEBUG option

--- blog/app.py
+++ blog/app.py
@@ -3,14 +3,14 @@
 
 import os.path
 
-import tornado.httpserver
+import tornado.web
 import tornado.ioloop
 import tornado.options
-import tornado.web
+import tornado.httpserver
 from tornado.options import define, options
 
 from libs import sqlite3lib
-from config import COOKIE_SECRET, DATABASE
+from config import COOKIE_SECRET, DATABASE, DEBUG
 from blog import handlers as handler
 
 define("port", default=8888, help="run on the given port", type=int)
@@ -20,13 +20,13 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = handler
         settings = dict(
-            template_path = os.path.join(os.path.dirname(__file__), "templates"), 
-            static_path = os.path.join(os.path.dirname(__file__), "static"), 
-            xsrf_cookies = True, 
-            cookie_secret = COOKIE_SECRET, 
-            login_url = "/auth/signin", 
-            autoescape = None, 
-            debug = True, 
+            template_path = os.path.join(os.path.dirname(__file__), "templates"),
+            static_path = os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies = True,
+            cookie_secret = COOKIE_SECRET,
+            login_url = "/auth/signin",
+            autoescape = None,
+            debug = DEBUG,
         )
         tornado.web.Application.__init__(self, handlers, **settings)
         self.db = sqlite3lib.Connection(DATABASE)
