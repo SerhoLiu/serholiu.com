fe43110a57ee1e105e7ea8356c4e251d76bec0d6
Serho Liu <serholiu@gmail.com>
2014-01-29 16:20:24 +0800

    encoding fix

--- miniakio/blog.py
+++ miniakio/blog.py
@@ -175,7 +175,7 @@ class PickyDownHandler(BaseHandler):
     def get(self, slug):
         mdfile = PICKY_DIR + "/" + str(slug)
         try:
-            md = open(mdfile)
+            md = open(mdfile, "r", encoding="utf-8")
         except IOError:
             self.abort(404)
         markdown = md.read()
