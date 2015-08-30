615b6edc0711dc0642de6a4614fd88668961383d
Serho Liu <serholiu@gmail.com>
2015-08-30 10:03:12 +0800

    格式修改

--- blogconfig.py
+++ blogconfig.py
@@ -3,16 +3,20 @@
 
 import os
 
+
 # 用于 cookie 加密，请换一个随机的，足够长，足够复杂的字符串
 # !!! 不要使用现在这个
 COOKIE_SECRET = "May the Force Be with you"
+
 
 # 数据库文件路径，默认是和配置文件同目录
 # eg. DATABASE = "/home/myblog/mydb.db"
 DATABASE = os.path.join(os.path.dirname(__file__), "newblog.db")
 
+
 # 你的博客名
 SITE_NAME = "I'm Force"
+
 
 # Picky 目录路径，默认和配置文件同目录
 PICKY_DIR = os.path.join(os.path.dirname(__file__), "picky")
