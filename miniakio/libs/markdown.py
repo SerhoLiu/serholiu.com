fe5b63e0cd22812da10659ccddcbf648b5ac5893
Serho Liu <serholiu@gmail.com>
2015-09-13 16:11:52 +0800

    remove debug

--- miniakio/libs/markdown.py
+++ miniakio/libs/markdown.py
@@ -136,7 +136,6 @@ class RenderMarkdownPost(object):
         meta = {"title": title.strip()}
         items = re.findall(r"<li>(.*?)</li>", header, re.S)
         for item in items:
-            print(item)
             key, value = item.split(":", 1)
             meta[key.strip()] = value.strip()
 
